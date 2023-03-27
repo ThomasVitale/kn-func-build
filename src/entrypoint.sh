a9991f8ad65196dd0628a1a976285395bc3141ff
qwpto <109690977+qwpto@users.noreply.github.com>
2023-03-27 02:40:56 +0200

    full-cf

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,4 +16,4 @@ echo ">>> Building function"
 FUNCTION_PATH=${4}
 FUNCTION_NAME=${5}
 FUNCTION_VERSION=${6}
-echo ${REGISTRY_USERNAME}$'\n'${REGISTRY_TOKEN}$'\n' |func build --builder=pack --builder-image=gcr.io/paketo-buildpacks/builder:full --path ${FUNCTION_PATH} --image ${REGISTRY_URL}/${FUNCTION_NAME}:${FUNCTION_VERSION} --push --verbose
+echo ${REGISTRY_USERNAME}$'\n'${REGISTRY_TOKEN}$'\n' |func build --builder=pack --builder-image=gcr.io/paketo-buildpacks/builder:full-cf --path ${FUNCTION_PATH} --image ${REGISTRY_URL}/${FUNCTION_NAME}:${FUNCTION_VERSION} --push --verbose
